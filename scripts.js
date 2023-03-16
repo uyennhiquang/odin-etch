790d4fe9ea499f891567bd43d6b94becf65e8a3b
Uyen Nhi Quang <111651301+uyennhiquang@users.noreply.github.com>
2023-03-15 20:42:16 -0400

    Make function dynamically allocate box's size

--- scripts.js
+++ scripts.js
@@ -5,15 +5,58 @@
 
 2. Repeat this. For each iteration, the outer loop has a row variable that's been appended. At the end of the inner loop (boxes completely generated), 
 */
-const documentBody = document.querySelector("body");
 
-for (let i = 0; i < 3; i++) {
-  const row = document.createElement("div");
-  row.classList.add("row");
-  for (let j = 0; j < 3; j++) {
-    const box = document.createElement("div");
-    box.classList.add("box");
-    row.appendChild(box);
+const canvasHTML = document.getElementById("canvas");
+const clearSketchHTML = document.getElementById("reset");
+const changeCanvasSizeHTML = document.getElementById("canvas-size");
+let canvasSize = 16;
+
+const generateCanvas = function (canvasSize) {
+  for (let i = 0; i < canvasSize; i++) {
+    const row = document.createElement("div");
+    row.classList.add("row");
+    for (let j = 0; j < canvasSize; j++) {
+      const box = document.createElement("div");
+      box.classList.add("box");
+      box.classList.add("selector");
+      box.style.padding = `${Math.floor(500 / canvasSize / 2)}px`;
+      if (j === 0) box.style.borderLeft = "1px solid black";
+      if (i === canvasSize - 1) box.style.borderBottom = "1px solid black";
+      row.appendChild(box);
+    }
+    canvasHTML.appendChild(row);
   }
-  documentBody.appendChild(row);
+};
+
+generateCanvas(canvasSize);
+
+changeCanvasSizeHTML.addEventListener("click", function () {
+  canvasSize = Number(prompt("Please put in a number for a square canvas"));
+  generateCanvas(canvasSize);
+});
+
+const boxHTML = document.querySelectorAll(".box");
+let isDrawing = false;
+
+canvasHTML.addEventListener("mousedown", function () {
+  isDrawing = true;
+});
+
+for (let i = 0; i < boxHTML.length; i++) {
+  boxHTML[i].addEventListener("mousemove", function () {
+    if (isDrawing) boxHTML[i].classList.add("colored");
+  });
+  boxHTML[i].addEventListener("mousedown", function () {
+    boxHTML[i].classList.add("colored");
+  });
 }
+
+window.addEventListener("mouseup", function () {
+  if (isDrawing) isDrawing = false;
+});
+
+reset.addEventListener("click", function () {
+  for (let i = 0; i < boxHTML.length; i++) {
+    boxHTML[i].classList.remove("colored");
+  }
+});
