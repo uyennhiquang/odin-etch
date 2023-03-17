94a6f3894b9ec3d7df1db01a2decf0c57ee776f0
Uyen Nhi Quang <111651301+uyennhiquang@users.noreply.github.com>
2023-03-16 20:30:17 -0400

    Add finishing touch

--- scripts.js
+++ scripts.js
@@ -5,13 +5,16 @@
 
 2. Repeat this. For each iteration, the outer loop has a row variable that's been appended. At the end of the inner loop (boxes completely generated), 
 */
-
-const canvasHTML = document.getElementById("canvas");
 const clearSketchHTML = document.getElementById("reset");
 const changeCanvasSizeHTML = document.getElementById("canvas-size");
-let canvasSize = 16;
+const sectionCanvasHTML = document.getElementById("section--canvas");
 
 const generateCanvas = function (canvasSize) {
+  // Canvas generation
+  const canvasHTML = document.createElement("div");
+  canvasHTML.setAttribute("id", "canvas");
+  sectionCanvasHTML.appendChild(canvasHTML);
+
   for (let i = 0; i < canvasSize; i++) {
     const row = document.createElement("div");
     row.classList.add("row");
@@ -26,37 +29,41 @@ const generateCanvas = function (canvasSize) {
     }
     canvasHTML.appendChild(row);
   }
+
+  // Sketching code
+  const boxHTML = document.querySelectorAll(".box");
+  let isDrawing = false;
+
+  for (let i = 0; i < boxHTML.length; i++) {
+    boxHTML[i].addEventListener("mousedown", function () {
+      isDrawing = true;
+    });
+
+    boxHTML[i].addEventListener("mousemove", function () {
+      if (isDrawing) boxHTML[i].classList.add("colored");
+    });
+    boxHTML[i].addEventListener("mousedown", function () {
+      boxHTML[i].classList.add("colored");
+    });
+  }
+
+  window.addEventListener("mouseup", function () {
+    if (isDrawing) isDrawing = false;
+  });
+
+  reset.addEventListener("click", function () {
+    for (let i = 0; i < boxHTML.length; i++) {
+      boxHTML[i].classList.remove("colored");
+    }
+  });
 };
 
-generateCanvas(canvasSize);
+generateCanvas(16);
 
 changeCanvasSizeHTML.addEventListener("click", function () {
-  canvasSize = Number(prompt("Please put in a number for a square canvas"));
+  do {
+    canvasSize = Number(prompt("Please put in a number for a square canvas"));
+  } while (canvasSize <= 0 || canvasSize > 100 || isNaN(canvasSize));
+  document.getElementById("canvas").remove();
   generateCanvas(canvasSize);
-});
-
-const boxHTML = document.querySelectorAll(".box");
-let isDrawing = false;
-
-canvasHTML.addEventListener("mousedown", function () {
-  isDrawing = true;
-});
-
-for (let i = 0; i < boxHTML.length; i++) {
-  boxHTML[i].addEventListener("mousemove", function () {
-    if (isDrawing) boxHTML[i].classList.add("colored");
-  });
-  boxHTML[i].addEventListener("mousedown", function () {
-    boxHTML[i].classList.add("colored");
-  });
-}
-
-window.addEventListener("mouseup", function () {
-  if (isDrawing) isDrawing = false;
-});
-
-reset.addEventListener("click", function () {
-  for (let i = 0; i < boxHTML.length; i++) {
-    boxHTML[i].classList.remove("colored");
-  }
 });
